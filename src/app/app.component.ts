ae47059faaaff0f77d5bbe2df25bb2a78e345b75
Eduard Puchaew <jamool@gmx.de>
2024-10-05 13:47:59 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, AboutmeComponent, MyskillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfoliowebsite';
+  
 }
---
+++ src/app/myskills/myskills.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-myskills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './myskills.component.html',
+  styleUrl: './myskills.component.scss'
+})
+export class MyskillsComponent {
+
+  myskills = [
+    { imgSrc: '../../assets/img/myskills/html.png', name: 'HTML' },
+    { imgSrc: '../../assets/img/myskills/css.png', name: 'CSS' },
+    { imgSrc: '../../assets/img/myskills/javascript.png', name: 'JavaScript' },
+    { imgSrc: '../../assets/img/myskills/typescript.png', name: 'TypeScript' },
+    { imgSrc: '../../assets/img/myskills/angular.png', name: 'Angular' },
+    { imgSrc: '../../assets/img/myskills/firebase.png', name: 'Firebase' },
+    { imgSrc: '../../assets/img/myskills/git.png', name: 'Git' },
+    { imgSrc: '../../assets/img/myskills/api.png', name: 'API' },
+    { imgSrc: '../../assets/img/myskills/scrum.png', name: 'Scrum' },
+    { imgSrc: '../../assets/img/myskills/materialdesign.png', name: 'Material Design' },
+    { imgSrc: '../../assets/img/myskills/continuallylearning.png', name: 'Continually Learning' }
+  ];
+
+  
+
+}
