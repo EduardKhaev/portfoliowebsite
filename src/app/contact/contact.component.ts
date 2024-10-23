ed38511203719afd54c2c4637821ef251bdf4c1f
Eduard Puchaew <jamool@gmx.de>
2024-10-23 21:14:38 +0200

    save response reference and contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -22,48 +22,7 @@ export class ContactComponent {
     message:"",
     checkbox: false
   }
-
-  onFocus(input: any) {
-    input.classList.remove('invalid');
-  }
-  
-  onBlur(input: any) {
-    // Nutze Angular's Validierung direkt
-    if (input.valid) {
-      input.classList.add('valid');
-      input.classList.remove('invalid');
-    } else {
-      input.classList.add('invalid');
-      input.classList.remove('valid');
-    }
-  }
-
-  mailTest = true;
-
-  post = {
-    endPoint: 'https://deineDomain.de/sendMail.php',
-    body: (payload: any) => JSON.stringify(payload),
-    options: {
-      headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
-      },
-    },
-  };
-
-  onSubmit(ngForm: NgForm) {
-    // Überprüfen, ob das Formular korrekt ausgefüllt ist
-    if (ngForm.submitted && ngForm.form.valid) {
-      // Beispiel: POST-Request senden, um Daten zu übermitteln
-      this.http.post('https://example.com/sendMail.php', this.contactData).subscribe({
-        next: (response) => {
-          console.log('Form successfully submitted!', response);
-          ngForm.resetForm(); // Formular zurücksetzen
-        },
-        error: (error) => {
-          console.error('Error submitting form:', error);
-        }
-      });
-    }
+  onSubmit(){
+  console.log("läuft");
   }
 }
